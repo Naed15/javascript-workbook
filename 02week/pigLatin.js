aed84912364e04a821300eb7bf3f5b90861aa5f4
nae deshazer <naedeshazer@naes-MacBook-Pro.local>
2019-07-02 20:20:45 -0500

    new piglatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,28 +8,54 @@ const rl = readline.createInterface({
 });
 
 function pigLatin(word) {
-	return word.split("");
+	word = word.trim().toLowerCase();
+	function convert(word) {
+		const vowels = ["a", "e", "i", "o", "u", "y"];
+		const splitWord = word
+			.toLowerCase()
+			.trim()
+			.split(""); //['c','a','r']
+
+		const ending = new Array("a", "y");
+		const firstLetter = splitWord[0];
+		word = word.toLowerCase().split(" ");
+
+		if (vowels.includes(firstLetter)) {
+			//is the first letter a vowel?
+			const myTranslation = word + "yay"; //then add 'yay' and we are done!
+			myTranslation[0].toUpperCase();
+			return myTranslation;
+		} else {
+			splitWord.push(splitWord.shift()); //remove the first consonant, send to end
+
+			for (let i = 0; i < splitWord.length; i++) {
+				//check the next letters (is or isNotVowel)
+				if (!vowels.includes(splitWord[i])) {
+					//if not vowel
+					splitWord.push(splitWord.shift()); //remove the first vowel, sent to end.
+				} else {
+					const myTranslation = splitWord.join("");
+					myTranslation[0].toUpperCase();
+					return myTranslation.concat("ay");
+				}
+			}
+		}
+	}
+	//Lets get started with the processing
+	if (word.split(" ").length > 1) {
+		//if it's more than 1 word
+		let mySplitPhrase = word.split(" "); // then split up the words
+		let myTranslations = [];
+		// return convert(word);
+		for (let i = 0; i < mySplitPhrase.length; i++) {
+			//for each of the words (mySplitPhrase[i])
+			myTranslations.push(convert(mySplitPhrase[i]));
+		}
+		return myTranslations.join(" ");
+	} else {
+		return convert(word); //only one word, so translate once!
+	}
 }
-
-function pigLatin(word) {
-	let chars = letters(word);
-	return chars.slice(1).join("") + chars[0] + "ay";
-}
-
-function pigLatin(word) {
-	return sentence.replace(pigLatin);
-}
-
-pigLatin("car"), "arcay";
-pigLatin("dog"), "ogday";
-pigLatin("create"), "eatecray";
-pigLatin("valley"), "alleyvay";
-
-pigLatin("egg"), "eggyay";
-pigLatin("emission"), "emissionyay";
-pigLatin("HeLlO "), "ellohay";
-" RoCkEt", "ocketray";
-
 function getPrompt() {
 	rl.question("word ", answer => {
 		console.log(pigLatin(answer));
