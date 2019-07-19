5facb19f464bd5e8483e740f9c772205ccdce577
nae deshazer <naedeshazer@naes-MacBook-Pro.local>
2019-07-18 20:51:02 -0500

    MarsMission1

---
+++ 04week/loop.js
@@ -0,0 +1,27 @@
+let x = 1;
+
+do {
+	// Increment variable by 1
+	x++;
+	console.log(x);
+} while (x < 1000);
+
+let person = {
+	firstName: "Jane",
+	lastName: "Doe",
+	birthDate: "Jan 5, 1927",
+	gender: "female"
+};
+
+let value = person.birthDate[person.birthDate.length - 1];
+
+console.log(value);
+
+console.log(typeof person.birthDate[person.birthDate.length - 1]);
+
+let arrayOfPersons = {
+	firstName: "Jane",
+	lastName: "Doe",
+	birthDate: "Jan 5, 1927",
+	gender: "female"
+};
